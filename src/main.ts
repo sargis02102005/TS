0441da9e6674e09e5ddb738ee2b17d32731b7a16
Саргис <sargis.oganyan.04@mail.ru>
2025-10-13 18:22:03 +0300

    Базовые типы(TS):Укажите типы не даделан в type A = { info?:string[] | number[]; }

---
+++ src/main.ts
@@ -0,0 +1,32 @@
+type A = {
+  age?: number | string;
+  nick?: string;
+  info?: string[] | number[];
+  photo?: string | null;
+};
+
+const a1: A = {
+  age: 10,
+  nick: 'mynick',
+  info: [1, 100],
+  photo: 'string',
+};
+
+const a2: A = {
+  age: '10 years',
+  nick: '',
+  info: { id: 100 },
+  photo: null,
+};
+
+const a3: A = {
+  nick: '       ',
+  info: ['secret', 'key'],
+};
+
+const a4: A = {
+  nick: '',
+  info: {},
+};
+
+console.log(a1, a2, a3, a4);
