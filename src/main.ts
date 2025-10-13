634db16e8c77502f8ce29501fd034b27d48ff481
Саргис <sargis.oganyan.04@mail.ru>
2025-10-13 20:28:52 +0300

    Базовые типы(TS):Список покупок

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,79 @@
-const func = (src: Array<Record<string, number>>) => {
-  let sum = 0;
+/*
+Напишите функцию, которая получает на вход семью и выводит в консоль список покупок этой семьи.
+Если название продукта имеет чётное количество знаков - выведите название ЗАГЛАВНЫМИ БУКВАМИ, например
 
-  for (const obj of src) {
-    for (const number of Object.values(obj)) {
-      sum += number;
-    }
-  }
+Опишите типы:
+* Product
+* Person
+* Family
 
-  return sum;
+Часть данных вам уже дана, осталось доработать функцию.
+Вывод программы должен быть таким же, как в скриншоте ниже.
+
+ */
+
+type Product = {
+  name: string;
+  count: number;
 };
 
-console.log(func([{ a: 20, b: 30 }, {}, { a: 3 }, { x: 5 }])); // 58
+type Person = {
+  name: string;
+  products: Product[];
+};
+
+type Family = {
+  name: string;
+  persons: Person[];
+};
+
+const family: Family = {
+  name: 'Алексеевы',
+  persons: [
+    {
+      name: 'Отец',
+      products: [
+        { name: 'Кофе', count: 2 },
+        { name: 'Колбаса', count: 3 },
+        { name: 'Огурцы', count: 3 },
+      ],
+    },
+    {
+      name: 'Мать',
+      products: [
+        { name: 'Молоко', count: 1 },
+        { name: 'Сыр', count: 74 },
+      ],
+    },
+    {
+      name: 'Дочь',
+      products: [
+        { name: 'Конфеты', count: 29 },
+        { name: 'Лимонад', count: 30 },
+        { name: 'Салат', count: 3 },
+        { name: 'Помидоры', count: 3 },
+      ],
+    },
+    {
+      name: 'Сын',
+      products: [{ name: 'Чипсы', count: 1 }],
+    },
+  ],
+};
+
+const printList = (family: Family) => {
+  console.log(`Список покупок семьи "${family.name}"\n`);
+
+  for (const person of family.persons) {
+    console.log(`Покупки ${person.name}:`);
+
+    for (const item of person.products) {
+      const name = item.name.length % 2 ? item.name : item.name.toUpperCase();
+
+      console.log(`  *  ${name} (${item.count})`);
+    }
+    console.log();
+  }
+};
+
+printList(family);
