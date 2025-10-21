552b1046205b193d6de152eacbe6b53e2741090b
Саргис <sargis.oganyan.04@mail.ru>
2025-10-21 21:29:13 +0300

    Степень двойки

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,32 @@
-type A = {
-  age?: number | string;
-  nick?: string;
-  info?: string[] | number[] | { id?: number };
-  photo?: string | null;
+const isPowerOf2 = (n: number): boolean => {
+  if (n <= 0) {
+    return false;
+  }
+
+  let stepen = 1;
+
+  while (stepen < n) {
+    stepen *= 2;
+  }
+  return stepen === n;
 };
 
-const a1: A = {
-  age: 10,
-  nick: 'mynick',
-  info: [1, 100],
-  photo: 'string',
-};
-
-const a2: A = {
-  age: '10 years',
-  nick: '',
-  info: { id: 100 },
-  photo: null,
-};
-
-const a3: A = {
-  nick: '       ',
-  info: ['secret', 'key'],
-};
-
-const a4: A = {
-  nick: '',
-  info: {},
-};
-
-console.log(a1);
-console.log(a2);
-console.log(a3);
-console.log(a4);
+console.log(isPowerOf2(0) === false);
+console.log(isPowerOf2(1) === true);
+console.log(isPowerOf2(2) === true);
+console.log(isPowerOf2(3) === false);
+console.log(isPowerOf2(4) === true);
+console.log(isPowerOf2(7) === false);
+console.log(isPowerOf2(8) === true);
+console.log(isPowerOf2(120) === false);
+console.log(isPowerOf2(127) === false);
+console.log(isPowerOf2(128) === true);
+console.log(isPowerOf2(500) === false);
+console.log(isPowerOf2(512) === true);
+console.log(isPowerOf2(777) === false);
+console.log(isPowerOf2(1024) === true);
+console.log(isPowerOf2(65000) === false);
+console.log(isPowerOf2(65536) === true);
+console.log(isPowerOf2(520000) === false);
+console.log(isPowerOf2(524288) === true);
+console.log(isPowerOf2(1000000) === false);
