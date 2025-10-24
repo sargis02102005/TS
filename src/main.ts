95802ab885631a120ffd90936b27d5bcb9c5a1aa
Саргис <sargis.oganyan.04@mail.ru>
2025-10-24 16:42:17 +0300

    Составные типы(TS):Строковый литерал

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,23 @@
-const isPowerOf2 = (n: number): boolean => {
-  let stepen = 1;
+import chalk from 'chalk';
 
-  while (stepen < n) {
-    stepen *= 2;
-  }
-  return stepen === n;
+type statusOrder = 'pending' | 'preparing' | 'delivering' | 'completed';
+
+type Order = {
+  id: number;
+  amount: number;
+  status: statusOrder;
 };
 
-console.log(isPowerOf2(0) === false);
-console.log(isPowerOf2(1) === true);
-console.log(isPowerOf2(2) === true);
-console.log(isPowerOf2(3) === false);
-console.log(isPowerOf2(4) === true);
-console.log(isPowerOf2(7) === false);
-console.log(isPowerOf2(8) === true);
-console.log(isPowerOf2(120) === false);
-console.log(isPowerOf2(127) === false);
-console.log(isPowerOf2(128) === true);
-console.log(isPowerOf2(500) === false);
-console.log(isPowerOf2(512) === true);
-console.log(isPowerOf2(777) === false);
-console.log(isPowerOf2(1024) === true);
-console.log(isPowerOf2(65000) === false);
-console.log(isPowerOf2(65536) === true);
-console.log(isPowerOf2(520000) === false);
-console.log(isPowerOf2(524288) === true);
-console.log(isPowerOf2(1000000) === false);
+const printOrder = (order: Order) => {
+  const messades = {
+    pending: chalk.yellow('Ожидает обработки'),
+    preparing: chalk.red('Готовится!'),
+    delivering: chalk.blue('Доставка!'),
+    completed: chalk.green('Выполнен!'),
+  };
+  console.log(chalk.bgBlack(`Заказ #${order.id}: ${messades[order.status]}`));
+};
+
+printOrder({ id: 1, amount: 500, status: 'preparing' });
+
+printOrder({ id: 6, amount: 2000, status: 'delivering' });
