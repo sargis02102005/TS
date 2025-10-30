7014550b4c6da7ba54cd984eb56878c041a90974
Саргис <sargis.oganyan.04@mail.ru>
2025-10-30 13:42:32 +0300

    функция joinWithCase(words, usingCase)

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,76 @@
-import chalk from 'chalk';
+/*
 
-type statusOrder = 'pending' | 'preparing' | 'delivering' | 'completed';
+Напишите функцию joinWithCase(words, usingCase), которая получает на вход 2 аргумента:
+1. words - массив слов, которые необходимо объединить в одно слово
+2. usingCase - название регистра, используя который необходимо объединить слова в одно
 
-type Order = {
-  id: number;
-  amount: number;
-  status: statusOrder;
-};
+Функция должна поддерживать 4 вида регистров:
+* PascalCase - первое и каждое новое слово с большой буквы
+* camelCase - первое слово с маленькой буквы, каждое следующее с большой
+* snake_case - каждое слово маленькими буквами через нижнее подчеркивание
+* kebab-case - каждое слово маленькими буквами через тире
 
-const printOrder = (order: Order) => {
-  let message: string;
+Пример использования:
+*/
+const joinWithCase = (words: string[], usingCase: string) => {
+  let wordstest = '';
+  let i = 0;
 
-  switch (order.status) {
-    case 'pending':
-      message = chalk.yellow('Ожидает обработки');
-      break;
-    case 'preparing':
-      message = chalk.cyan('Готовится!');
-      break;
-    case 'delivering':
-      message = chalk.blue('Доставка!');
-      break;
-    default:
-      message = chalk.green('Выполнен!');
+  if (usingCase === 'snake_case') {
+    for (const item of words) {
+      wordstest += item.toLowerCase();
+      if (i < words.length - 1) {
+        wordstest += '_';
+        i++;
+      }
+    }
   }
-  console.log(`Заказ #${order.id}: ${message}`);
+
+  if (usingCase === 'kebab-case') {
+    for (const item of words) {
+      wordstest += item.toLowerCase();
+      if (i < words.length - 1) {
+        wordstest += '-';
+        i++;
+      }
+    }
+  }
+
+  if (usingCase === 'PascalCase') {
+    for (const item of words) {
+      wordstest += item[0].toUpperCase() + item.slice(1).toLowerCase();
+    }
+  }
+
+  if (usingCase === 'camelCase') {
+    for (const item of words) {
+      if (wordstest.length === 0) {
+        wordstest += item.toLowerCase();
+      } else {
+        wordstest += item[0].toUpperCase() + item.slice(1).toLowerCase();
+      }
+    }
+  }
+  return wordstest;
 };
 
-printOrder({ id: 1, amount: 500, status: 'preparing' });
+const testCase1 = ['path'];
 
-printOrder({ id: 6, amount: 2000, status: 'delivering' });
+console.log(joinWithCase(testCase1, 'PascalCase')); // Path
+console.log(joinWithCase(testCase1, 'camelCase')); // path
+console.log(joinWithCase(testCase1, 'snake_case')); // path
+console.log(joinWithCase(testCase1, 'kebab-case')); // path
+
+const testCase2 = ['favorite', 'COLOR'];
+
+console.log(joinWithCase(testCase2, 'PascalCase')); // FavoriteColor
+console.log(joinWithCase(testCase2, 'camelCase')); // favoriteColor
+console.log(joinWithCase(testCase2, 'snake_case')); // favorite_color
+console.log(joinWithCase(testCase2, 'kebab-case')); // favorite-color
+
+const testCase3 = ['uSEr', 'FIRST', 'Login', 'dATE'];
+
+console.log(joinWithCase(testCase3, 'PascalCase')); // UserFirstLoginDate
+console.log(joinWithCase(testCase3, 'camelCase')); // userFirstLoginDate
+console.log(joinWithCase(testCase3, 'snake_case')); // user_first_login_date
+console.log(joinWithCase(testCase3, 'kebab-case')); // user-first-login-date
