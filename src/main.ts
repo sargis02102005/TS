bec3120370ac2228c796286b8e636f1ab4456262
Саргис <sargis.oganyan.04@mail.ru>
2025-11-05 19:13:04 +0300

    Лайвкодинг “средняя температура”

--- src/main.ts
+++ src/main.ts
@@ -1,50 +1,90 @@
-const joinWithCase = (words: string[], usingCase: string) => {
-  let result = '';
+/*
+Дано:
+Больница, имеющая список палат, в каждой палате список пациентов.
 
-  if (usingCase === 'snake_case' || usingCase === 'kebab-case') {
-    const symbol = usingCase === 'snake_case' ? '_' : '-';
-    return words.join(symbol).toLowerCase();
-  }
+Каждая палата содержит информацию о:
+* Номер палаты
+* Список пациентов
 
-  if (usingCase === 'PascalCase') {
-    for (const item of words) {
-      result += item[0].toUpperCase() + item.slice(1).toLowerCase();
-    }
-    return result;
-  }
+Каждый пациент содержит информацию о:
+* Своём имени
+* Температуре
 
-  if (usingCase === 'camelCase') {
-    for (const item of words) {
-      if (result.length === 0) {
-        result += item.toLowerCase();
-      } else {
-        result += item[0].toUpperCase() + item.slice(1).toLowerCase();
-      }
-    }
-    return result;
-  }
-  return 'Ошибка, такова вида регистра не существует!';
+Ваша задача, вывести список палат с указанием средней температуры в каждой палате,
+а так же с указанием средней температуры по всей больнице.
+
+*/
+
+type Patient = {
+  // описать тип
+  name: string;
+  temp: number;
 };
 
-console.log(joinWithCase(['хочу', 'соединить', 'эти', 'пять', 'слов'], 'pascal_case'));
+type Room = {
+  // описать тип
+  name: string;
+  patients: Patient[];
+};
 
-const testCase1 = ['path'];
+// По всей больнице средняя температура 37.792 (пустые палаты в расчет не идут, температура округлена до 3х знаков после запятой)
+const hospital: Room[] = [
+  {
+    name: 'A-123', // Средняя температура 36.6
+    patients: [
+      { name: 'Мария', temp: 36.8 },
+      { name: 'Валентин', temp: 36.4 },
+    ],
+  },
+  {
+    name: 'B-093', // Средняя температура 40
+    patients: [{ name: 'Алексей', temp: 40.0 }],
+  },
+  {
+    name: 'C-107', // Средняя температура 0
+    patients: [],
+  },
+  {
+    name: 'D-246', // Средняя температура 36.775
+    patients: [
+      { name: 'Михаил', temp: 34.9 },
+      { name: 'Анатолий', temp: 38.9 },
+      { name: 'Гарик', temp: 37 },
+      { name: 'Антон', temp: 36.3 },
+    ],
+  },
+];
 
-console.log(joinWithCase(testCase1, 'PascalCase')); // Path
-console.log(joinWithCase(testCase1, 'camelCase')); // path
-console.log(joinWithCase(testCase1, 'snake_case')); // path
-console.log(joinWithCase(testCase1, 'kebab-case')); // path
+const calculateTemperature = (rooms: Room[]) => {
+  let sumTemp = 0;
+  let sumCount = 0;
+  for (const room of rooms) {
+    if (room.patients.length === 0) {
+      continue;
+    }
+    let result = 0;
 
-const testCase2 = ['favorite', 'COLOR'];
+    for (const patient of room.patients) {
+      result += patient.temp;
+      sumTemp += patient.temp;
+    }
+    sumCount += room.patients.length;
+    const roomAverage = result / room.patients.length;
+    const roundedRoomAverage = Math.round(roomAverage * 1000) / 1000;
 
-console.log(joinWithCase(testCase2, 'PascalCase')); // FavoriteColor
-console.log(joinWithCase(testCase2, 'camelCase')); // favoriteColor
-console.log(joinWithCase(testCase2, 'snake_case')); // favorite_color
-console.log(joinWithCase(testCase2, 'kebab-case')); // favorite-color
+    console.log(`Палата ${room.name}: ${roundedRoomAverage} C`);
+  }
+  const roomhospital = sumTemp / sumCount;
+  const roundedHospitalAverage = Math.round(roomhospital * 1000) / 1000;
 
-const testCase3 = ['uSEr', 'FIRST', 'Login', 'dATE'];
+  console.log(`Вся больница: ${roundedHospitalAverage} C`);
+};
 
-console.log(joinWithCase(testCase3, 'PascalCase')); // UserFirstLoginDate
-console.log(joinWithCase(testCase3, 'camelCase')); // userFirstLoginDate
-console.log(joinWithCase(testCase3, 'snake_case')); // user_first_login_date
-console.log(joinWithCase(testCase3, 'kebab-case')); // user-first-login-date
+calculateTemperature(hospital);
+// Ожидаемый вывод программы:
+/*
+Палата [A-123]: 36.6 C
+Палата [B-093]: 40 C
+Палата [D-246]: 36.775 C
+Вся больница: 37.792 C
+*/
