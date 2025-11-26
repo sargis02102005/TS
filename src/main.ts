5f3a3c51889525cf8349375b3da400a11be45a07
Саргис <sargis.oganyan.04@mail.ru>
2025-11-26 20:27:48 +0300

    Лайвкодинг - сравнение массивов в обратном порядке переделан

--- src/main.ts
+++ src/main.ts
@@ -1,48 +1,16 @@
-/*
-Фраза
-the quick brown fox jumps over the lazy dog
+const src = (a: number[], b: number[]) => {
+  if (a.length !== b.length) {
+    return false;
+  }
 
-содержит все буквы английского алфавита.
-
-Я перемешал обычный алфавит и создал свой алфавит, который замещает одни английские буквы другими. Вот та же самая фраза, но записанная с помощью нового алфавита:
-oak lgypb wited zts qgfch tuki oak mjrn xtv
-В данном примере буква t была изменена на o, буква h на a и так далее. Гарантируется отсутствие дубликатов, то есть одна буква нового алфавита соответствует строго одной букве старого алфавита и наоборот.
-
-Используя эти данные, расшифруйте фразу ниже.
-ntg ajuk fjbydv vikjo citvikhh yd mkjidydv qjujhpiyco. ptdvijoh!
-you
-
-Если в фразе встретились символы, которые не удалось декодировать - оставьте эти символы без изменения.
-
-Верное решение задачи должно показать человеко-читаемый понятный текст!
-
-
- */
-
-const shifrs = () => {
-  const fraza = 'the quick brown fox jumps over the lazy dog';
-  const shif = 'oak lgypb wited zts qgfch tuki oak mjrn xtv';
-  const frazza = 'ntg ajuk fjbydv vikjo citvikhh yd mkjidydv qjujhpiyco. ptdvijoh!';
-
-  let relt = '';
-
-  let i = 0;
-
-  while (relt.length < frazza.length) {
-    let s = 0;
-    while (s < shif.length) {
-      if (frazza[i] === shif[s]) {
-        relt += fraza[s];
-        i++;
-      }
-      if (frazza[i] === '.' || frazza[i] === '!') {
-        relt += frazza[i];
-        i++;
-      }
-      s++;
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[b.length - 1 - i]) {
+      return false;
     }
   }
-  console.log(relt);
+  return true;
 };
 
-shifrs();
+console.log(src([1], [1]));
+console.log(src([1, 2, 3, 4, 5, 6, 7, 8, 9, 0], [0, 9, 8, 7, 6, 5, 4, 3, 2, 1]));
+console.log(src([1, 2, 3], [3, 2, 1]));
