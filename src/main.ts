e4c4de03821075f175568a98ceb56f9f26ade207
Саргис <sargis.oganyan.04@mail.ru>
2025-12-02 17:11:52 +0300

    Продвинутые функции (TS):Самопроверка

--- src/main.ts
+++ src/main.ts
@@ -1,62 +1,28 @@
-/*
-Напишите ф-цию generatePassword(options), которая генерирует пароль согласно переданным настройкам.
+const array = [
+  [1, 9, 9],
+  [2, 3, 6],
+  [5, 5, 25],
+  [8, 3, 24],
+  [0, 0, 0],
+];
 
-Пока что поддерживается только одна настройка:
-1. Длина пароля
+const multiply = (a: number, b: number) => a * b;
 
-Для начала, базовая функция работает следующим образом:
-Она генерирует случайный пароль заданной длины,
-при этом пароль может состоять из маленьких, больших букв и цифр.
+const arrayMultiply = (arr: number[][]) => {
+  const results = [];
 
+  for (const items of arr) {
+    const a = items[0];
+    const b = items[1];
+    const product = multiply(a, b);
 
-Далее поэтапно добавить поддержку следующий настроек:
-1. Большие буквы (разрешить / запретить)
-2. Маленькие буквы (разрешить / запретить)
-3. Цифры (разрешить / запретить)
-4. Специальные символы (разрешить / запретить)
-
- */
-
-// Пример использования:
-import { faker } from '@faker-js/faker';
-
-type Options = {
-  length: number;
-  uppercase?: boolean;
-  lowercase?: boolean;
-  numbers?: boolean;
-  symbols?: boolean;
-};
-
-const generatePassword = (options: Options) => {
-  const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
-  const numberChars = '0123456789';
-  const symbolChars = '!@#$%^&*()_+-=[]{}|;:,.<>?';
-
-  let password = '';
-  for (let i = 0; i < options.length; i++) {
-    let allowedChars = '';
-    if (options.uppercase === true) {
-      const randomIndex1 = faker.number.int({ min: 0, max: uppercaseChars.length - 1 });
-      allowedChars += uppercaseChars[randomIndex1];
+    if (product === items[2]) {
+      results.push([`Результат: ${product}, Ожидаемый результат: ${items[2]}`]);
+    } else {
+      results.push([`Результаты не совпали❌`]);
     }
-    if (options.lowercase === true) {
-      const randomIndex2 = faker.number.int({ min: 0, max: lowercaseChars.length - 1 });
-      allowedChars += lowercaseChars[randomIndex2];
-    }
-    if (options.numbers === true) {
-      const randomIndex3 = faker.number.int({ min: 0, max: numberChars.length - 1 });
-      allowedChars += numberChars[randomIndex3];
-    }
-    if (options.symbols === true) {
-      const randomIndex4 = faker.number.int({ min: 0, max: symbolChars.length - 1 });
-      allowedChars += symbolChars[randomIndex4];
-    }
-    const randomIndex = faker.number.int({ min: 0, max: allowedChars.length - 1 });
-    password += allowedChars[randomIndex];
   }
-  return password;
+  return results;
 };
 
-console.log(generatePassword({ length: 10, uppercase: true, lowercase: true, numbers: true, symbols: true }));
+console.log(arrayMultiply(array));
