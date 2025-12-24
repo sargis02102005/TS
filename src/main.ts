3b62009e46168413fd6b9b5a1dfebdd0959ab8fc
Саргис <sargis.oganyan.04@mail.ru>
2025-12-24 12:22:49 +0300

    Работа с массивами(TS):Что выведет программа

--- src/main.ts
+++ src/main.ts
@@ -1,59 +1,10 @@
-type User = {
-  id: number;
-  name: string;
-  age: number;
-};
+const storage = [
+  { age: 10, name: 'first' },
+  { age: 20, name: 'second' },
+  { age: 30, name: 'third' },
+  { age: 40, name: 'fourth' },
+];
 
-type Car = {
-  id: number;
-  title: string;
-  vin: string;
-  serial: string;
-  pts: Pts;
-};
+const smalls = storage.map((s) => s.age > 20);
 
-type Pts = {
-  id: number;
-  vin: string;
-  owners: User[];
-};
-
-const adultOwners = (car: Car) => {
-  const adultOwners = car.pts.owners.filter((owner) => owner.age >= 18);
-
-  const ownersInfo = adultOwners.map((owner) => `${owner.name} (${owner.age})`);
-
-  return `Автомобиль "${car.title}"
-VIN: ${car.vin}
-Гос. номер: ${car.serial}
-Совершеннолетние владельцы: ${ownersInfo}`;
-};
-
-const prius: Car = {
-  id: 38,
-  title: 'Toyota Prius',
-  vin: 'JTDKARFP9L3128187',
-  serial: 'А777МР97',
-  pts: {
-    id: 849325,
-    vin: 'JTDKARFP9L3128187',
-    owners: [
-      { id: 3401, name: 'Лёха Перекуп', age: 35 },
-      { id: 1946, name: 'Вячеслав', age: 14 },
-      { id: 9613, name: 'Стас', age: 7 },
-      { id: 9613, name: 'Татьяна', age: 18 },
-      { id: 6542, name: 'Стас друг Лёхи Перекупа', age: 73 },
-      { id: 6542, name: 'Валентин', age: 73 },
-    ],
-  },
-};
-
-const res = adultOwners(prius);
-console.log(res);
-
-/*
-Автомобиль "Toyota Prius"
-VIN: JTDKARFP9L3128187
-Гос. номер: А777МР97
-Совершеннолетние владельцы: Лёха Перекуп (35), Татьяна (18), Валентин (73)
- */
+console.log(smalls); // [ false, false, true, true ]
