8bf5d7e739acbaae31658b688c8ef033246f180a
Саргис <sargis.oganyan.04@mail.ru>
2026-05-09 21:51:06 +0300

    Работа с файлами:Стихотворение

--- src/main.ts
+++ src/main.ts
@@ -1,60 +1,21 @@
-import { readFileSync } from 'fs';
+/*
+Вам дано стихотворение.
+Скопируйте его себе в текстовый файл.
 
-const users = [];
+Необходимо в нём все буквы "а" заменить на буквы "о" и обновлённое стихотворение записать в новый файл.
+Замена должна быть и для заглавных, и для прописных букв.
 
-// ... Вот здесь ваш код ...
-const readData = readFileSync('./users.csv', 'utf8').split('\n');
+✦ 🔥 Усложнённая версия (необязательно) - замените буквы "о" на "а" и "а" на "о"
+То есть, поменяйте буквы "а" и "о" местами. Обновлённое стихотворение запишите в новый файл.
 
-for (let i = 1; i < readData.length; i++) {
-  const [id, name, email, age] = readData[i].split(',');
-
-  users.push({ id: Number(id), name, email, age: Number(age) });
-}
-
-console.log(users);
-/* Вывод:
-[
-  {
-    id: 1,
-    name: 'Иван Петров',
-    email: 'ivan.pетров@email.ru',
-    age: 28
-  },
-  {
-    id: 2,
-    name: 'Мария Сидорова',
-    email: 'maria.sidorova@mail.ru',
-    age: 34
-  },
-  {
-    id: 3,
-    name: 'Алексей Иванов',
-    email: 'alex.ivanov@yandex.ru',
-    age: 22
-  },
-  {
-    id: 4,
-    name: 'Елена Козлова',
-    email: 'elena.kozlova@gmail.com',
-    age: 41
-  },
-  {
-    id: 5,
-    name: 'Дмитрий Смирнов',
-    email: 'dmitry.smirnov@outlook.ru',
-    age: 29
-  },
-  {
-    id: 6,
-    name: 'Ольга Кузнецова',
-    email: 'olga.kuznetsova@mail.ru',
-    age: 37
-  },
-  {
-    id: 7,
-    name: 'Сергей Морозов',
-    email: 'sergey.morozov@yandex.ru',
-    age: 25
-  }
-]
  */
+import { appendFileSync, readFileSync, writeFileSync } from 'node:fs';
+
+const content = readFileSync('./text.txt', 'utf8');
+
+writeFileSync('./append.txt', '', 'utf-8');
+for (const item of content.split('\n')) {
+  if (item.includes('а') || item.includes('А')) {
+    appendFileSync('./append.txt', item.replace(/а/g, 'о').replace(/А/g, 'О') + '\n', 'utf-8');
+  }
+}
